c6dcdb063c8b1a4b56145fa48d44886d32f326ec
Nate <hunter.j.nate@gmail.com>
2019-09-23 14:27:11 -0400

    Updates

---
+++ precache-manifest.1782ce5ba966ca1e5b02923c0850b01c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fd31bc42af5434968d8067bf1eea78",
+    "url": "/nyc-permits-viz/index.html"
+  },
+  {
+    "revision": "682a2ddb1f9145e819e2",
+    "url": "/nyc-permits-viz/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7030f223f75220be9047",
+    "url": "/nyc-permits-viz/static/css/main.98ccaa15.chunk.css"
+  },
+  {
+    "revision": "682a2ddb1f9145e819e2",
+    "url": "/nyc-permits-viz/static/js/2.3547912b.chunk.js"
+  },
+  {
+    "revision": "7030f223f75220be9047",
+    "url": "/nyc-permits-viz/static/js/main.d2517d98.chunk.js"
+  },
+  {
+    "revision": "6cbed469b034ce852176",
+    "url": "/nyc-permits-viz/static/js/runtime-main.c3db2fda.js"
+  },
+  {
+    "revision": "86333227bf68260e7fa7c15e3be2056f",
+    "url": "/nyc-permits-viz/static/media/2018.86333227.csv"
+  },
+  {
+    "revision": "7c2db490f62297e50afdae87651221ac",
+    "url": "/nyc-permits-viz/static/media/2019.7c2db490.csv"
+  },
+  {
+    "revision": "957c4dd9aa815cc1de2040cc1259589f",
+    "url": "/nyc-permits-viz/static/media/GitHub-Logo-03.957c4dd9.png"
+  },
+  {
+    "revision": "75604377dd3fd0b990d6c822f2e78c5d",
+    "url": "/nyc-permits-viz/static/media/Logo-twitter.75604377.png"
+  },
+  {
+    "revision": "228bdb809d2c5ab931cb8a9ebf0511bd",
+    "url": "/nyc-permits-viz/static/media/NYCOpenData_Logo.228bdb80.png"
+  },
+  {
+    "revision": "0958f2678826754e0a1752efc2140a1e",
+    "url": "/nyc-permits-viz/static/media/New_York_City_Department_of_Environmental_Protection_logo.0958f267.png"
+  },
+  {
+    "revision": "ae5cea694433a65ed34b47de35034926",
+    "url": "/nyc-permits-viz/static/media/all.ae5cea69.csv"
+  },
+  {
+    "revision": "80016fe31a86b138753e46df5eafd11d",
+    "url": "/nyc-permits-viz/static/media/totals.80016fe3.csv"
+  }
+]);
